9053e6a58a1a95bfff462d30b1e62a6c35c66893
Andrew Chon <andrewdchon@gmail.com>
2023-01-29 22:59:01 -0600

    Add sql driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Struct to hold application-wide dependencies
@@ -16,11 +19,19 @@ type application struct {
 func main() {
 	// Parse cli flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:andrew@tcp(192.168.1.100:3306)/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Create logger for writing information
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -35,6 +46,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dns)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
