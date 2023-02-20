b8c1786dae1be652f1715e7a12a257b520c140bb
Andrew Chon <andrewdchon@gmail.com>
2023-02-20 09:17:57 -0600

    feat: Logout functionality and protected routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -205,5 +205,16 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
